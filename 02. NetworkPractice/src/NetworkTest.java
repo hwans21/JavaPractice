707dc2526480cdc1e51c7ce29eb2a21c169fd40a
hwans21 <hwanseo3747@gmail.com>
2021-12-12 23:17:17 +0900

    first commit

---
+++ 02. NetworkPractice/src/NetworkTest.java
@@ -0,0 +1,6 @@
+
+public class NetworkTest {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+}
---
+++ 03. NettyPractice/src/NettyTest.java
@@ -0,0 +1,6 @@
+
+public class NettyTest {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+}
